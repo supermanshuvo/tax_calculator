e2b41aa7b58a2623517b10adc0c7736878007ced
supermanshuvo <shuvothesuperman@gmail.com>
2021-08-30 19:46:55 +0600

    Change the code for add {}

--- src/components/InvestmentAllowance.jsx
+++ src/components/InvestmentAllowance.jsx
@@ -5,23 +5,25 @@ const InvestmentAllowance = () => {
         <h5 className="bg-secondary fw-light p-2">
           Investment Allowance and Tax Rebate
         </h5>
-        <div className="col-6">
-          <table>
+        <div className="container-fluid">
+          <table className="table">
             <tbody>
               <tr>
                 <td>Aontribution to Provident Fund</td>
-                <td>0.000</td>
+                <td>{0.0}</td>
               </tr>
               <tr>
                 <td>DPS/BSP/LIP/Others (If Applicable)</td>
-                <td>00.000</td>
+                <td>{0.0}</td>
               </tr>
-              <tr>
+              <tr className="fw-bold">
                 <td>Total Investment</td>
               </tr>
-              <tr>
+              <tr className="fw-bold">
                 <td>Allowed Investment</td>
-                <td>00.00 From 00.00</td>
+                <td>
+                  {0.0} From {0.0}
+                </td>
               </tr>
             </tbody>
           </table>
