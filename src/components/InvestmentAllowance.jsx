c5abf90992c06f3e44a4468a91a4ab6658374ae9
supermanshuvo <shuvothesuperman@gmail.com>
2021-08-30 18:53:35 +0600

    Remove unwanted css and add heading in investment Allowance file

--- src/components/InvestmentAllowance.jsx
+++ src/components/InvestmentAllowance.jsx
@@ -2,10 +2,12 @@ const InvestmentAllowance = () => {
   return (
     <div className="container mt-2">
       <div className="row">
-        <h5>Investment Allowance & Tax Rebate</h5>
+        <h5 className="bg-secondary fw-light p-2">
+          Investment Allowance and Tax Rebate
+        </h5>
         <div className="col-6">
           <table>
-            <thead></thead>
+            <tbody></tbody>
           </table>
         </div>
       </div>
