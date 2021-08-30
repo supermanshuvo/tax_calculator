20b8b6d2bc0eced0b3a41ef4f39571f1fc21a172
supermanshuvo <shuvothesuperman@gmail.com>
2021-08-30 18:44:58 +0600

    Set up file for the work done

---
+++ src/components/InvestmentAllowance.jsx
@@ -0,0 +1,16 @@
+const InvestmentAllowance = () => {
+  return (
+    <div className="container mt-2">
+      <div className="row">
+        <h5>Investment Allowance & Tax Rebate</h5>
+        <div className="col-6">
+          <table>
+            <thead></thead>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InvestmentAllowance;
