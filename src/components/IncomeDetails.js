8c936cb2172a76103524bbfc28cc7576c0b2f6e2
RakibulIslamRakib <rakibulr14@cse.pstu.ac.bd>
2021-09-02 01:06:34 -0700

    solve conflict in bonus calculaiton

--- src/components/IncomeDetails.js
+++ src/components/IncomeDetails.js
@@ -36,7 +36,7 @@ export const InComeDetails = (props)=>{
         calculateTaxable(inComeDetailsState.conveyancemAmount,inComeDetailsState.conveyancelAmount)+
         calculateTaxable(inComeDetailsState.livingmAmount,inComeDetailsState.livinglAmount)+
         calculateTaxable(inComeDetailsState.providentFundmAmount,inComeDetailsState.providentFundlAmount)+
-        calculateTaxable(inComeDetailsState.bonusmAmount,inComeDetailsState.bonuslAmount,2)+
+        calculateTaxable(inComeDetailsState.bonusmAmount,inComeDetailsState.bonuslAmount,bonusYoccurrance)+
         calculateTaxable(inComeDetailsState.specialAmountmAmount,inComeDetailsState.specialAmountlAmount,specialYoccurrance)+
         calculateTaxable(inComeDetailsState.othersmAmount,inComeDetailsState.otherslAmount,othersYoccurrance))
         return totalTaxable;
